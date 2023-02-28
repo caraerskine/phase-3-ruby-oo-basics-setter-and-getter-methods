474bfe143f3628c989813fc0bf8cf9ffad5b8426
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-02-28 15:55:40 -0500

    commit tree 9855e05b455a810a1f7a88cc889f0ba29560da7b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,44 @@
 class Dog
 
+    def name= (name)
+        @name = name
+    end
+
+    def breed= (breed)
+        @breed = breed
+    end
+
+    def name    
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
 end
+
+
+
+
+
+
+# class Dog
+
+# def name=(name)
+#         @name = name
+# end
+
+# def breed=(breed)
+#         @breed = breed
+# end
+
+# def name
+#     @name
+# end
+
+# def breed
+#     @breed
+# end
+
+# end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,59 @@
 class Person
 
+    def name=(name)
+        @name = name
+    end
+
+    def job= (job)
+        @job = job
+    end
+
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Person
+
+# def name=(name)
+#         @name = name
+# end
+
+# def job=(job)
+#     @job = job
+# end
+
+
+# def name
+#     @name
+# end
+
+# def job 
+#     @job
+# end
+
+# end
